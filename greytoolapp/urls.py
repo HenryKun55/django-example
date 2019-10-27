4435e8d479ffe18303f63724670e3ad7f8f9ad5b
Flavio Henrique <flavio@triider.com>
2019-10-27 20:01:09 -0300

    Adicionando e Removendo

--- greytoolapp/urls.py
+++ greytoolapp/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name='index'),
     path("<int:pk>/", views.project, name="project"),
-    path('add_project', views.addProject)
+    path('add', views.addProject, name="addProject"),
+    path('insert', views.addProjectButton, name="addProjectButton"),
+    path('<int:pk>/delete', views.rmProjectButton, name="rmProjectButton")
 ]
--- greytoolapp/views.py
+++ greytoolapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from greytoolapp.models import Project
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def index(request):
@@ -11,3 +12,22 @@ def project(request, pk):
     project = Project.objects.get(pk=pk)
     context = { 'project': project }
     return render(request, 'project_detail.html', context)
+
+def addProject(request):
+    return render(request, 'project_add.html')
+
+def addProjectButton(request):
+    if request.method=='POST':
+        post = request.POST
+        Project.objects.create(
+            title=post.get("titulo"), 
+            description=post.get("descricao"),
+            technology=post.get("tecnologia"),
+            image=post.get("foto_url")
+        )
+        return HttpResponseRedirect("/projects")
+
+def rmProjectButton(request, pk):
+    p = Project.objects.get(pk=pk)
+    p.delete()
+    return HttpResponseRedirect("/projects")
